3f435e8132672605b397d2476f4797508e61003d
Alexander Sack <asac@pantacor.com>
2021-10-22 21:35:28 +0200

    remove atom.mk from out/

--- atom.mk
+++ atom.mk
@@ -13,7 +13,8 @@ LOCAL_AUTOTOOLS_MAKE_BUILD_ARGS := CROSS_COMPILE=$(TARGET_CROSS) BUILD_CC=gcc
 LOCAL_AUTOTOOLS_MAKE_INSTALL_ARGS := lib=lib
 
 define LOCAL_AUTOTOOLS_CMD_CONFIGURE
-	cp -r $(mylocal)/*  $(call module-get-build-dir,libcap)/obj
+	cp -r $(PRIVATE_SRC_DIR)/*  $(call module-get-build-dir,libcap)/obj
+	rm -f $(call module-get-build-dir,libcap)/obj/atom.mk
 endef
 
 include $(BUILD_AUTOTOOLS)
